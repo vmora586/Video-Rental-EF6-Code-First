2f891b46239c806b7ba954106084f5038533b683
Victor M <victomc.586@gmail.com>
2020-06-14 18:41:37 -0500

    added video dto

---
+++ VideoRentalCodeFirst/Model/Video.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VideoRentalCodeFirst.Model
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
