db22066b67fb7d488993767ff22b77f11fdabc4d
Victor M <victomc.586@gmail.com>
2020-06-17 22:26:24 -0500

    populated Video table

---
+++ VideoRentalCodeFirst/Migrations/202006180319252_PopulateVideoTable.cs
@@ -0,0 +1,22 @@
+﻿namespace VideoRentalCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateVideoTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO VideoRentalCodeFirst.Videos(Name, ReleaseDate, Genre_Id) VALUES('Avatar', '2007/05/20', 3)");
+            Sql("INSERT INTO VideoRentalCodeFirst.Videos(Name, ReleaseDate, Genre_Id) VALUES('Jurassic Park', '2017/08/20', 3)");
+            Sql("INSERT INTO VideoRentalCodeFirst.Videos(Name, ReleaseDate, Genre_Id) VALUES('John Wick 3', '2018/02/15', 1)");
+            Sql("INSERT INTO VideoRentalCodeFirst.Videos(Name, ReleaseDate, Genre_Id) VALUES('Avengers Infinity War', '2018/06/15', 1)");
+            
+        }
+        
+        public override void Down()
+        {
+            Sql("TRUNCATE TABLE VideoRentalCodeFirst.Video");
+        }
+    }
+}
