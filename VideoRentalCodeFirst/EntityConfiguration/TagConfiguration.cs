1c99f79cd412ecf250d4bf327633ae73057d1aa9
Victor M <victomc.586@gmail.com>
2020-06-29 03:08:35 -0500

    added TagConfiguration class to add Tags table and to configure VideoTags many-to-many relationship

---
+++ VideoRentalCodeFirst/EntityConfiguration/TagConfiguration.cs
@@ -0,0 +1,21 @@
+using System.Data.Entity.ModelConfiguration;
+using VideoRentalCodeFirst.Model;
+
+namespace VideoRentalCodeFirst.EntityConfiguration
+{
+    public class TagConfiguration:EntityTypeConfiguration<Tag>
+    {
+        public TagConfiguration()
+        {
+            ToTable("Tags");
+
+            HasKey(t => t.Id);
+
+            HasMany(t => t.Videos)
+                .WithMany(v => v.Tags)
+                .Map(m => m.ToTable("VideoTags")
+                    .MapLeftKey("VideoId")
+                    .MapRightKey("TagId"));
+        }
+    }
+}
