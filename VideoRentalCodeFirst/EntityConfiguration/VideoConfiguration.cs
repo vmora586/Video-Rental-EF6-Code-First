8177e11b4fad9ef6147698d2cbb23ebb2c6e4eb7
Victor M <victomc.586@gmail.com>
2020-06-29 01:38:33 -0500

    added folder 'EntityConfiguration' and class 'VideoConfiguration' for starting using fluent Api

---
+++ VideoRentalCodeFirst/EntityConfiguration/VideoConfiguration.cs
@@ -0,0 +1,14 @@
+using System.Data.Entity.ModelConfiguration;
+using VideoRentalCodeFirst.Model;
+
+namespace VideoRentalCodeFirst.EntityConfiguration
+{
+    public class VideoConfiguration:EntityTypeConfiguration<Video>
+    {
+        public VideoConfiguration()
+        {
+                
+        }
+        
+    }
+}
