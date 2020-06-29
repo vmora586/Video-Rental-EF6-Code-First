9c71f7b3abc8a2ca5e43cea076aae83894cb9010
Victor M <victomc.586@gmail.com>
2020-06-29 02:51:16 -0500

    added navigation property Tags to map many to many relationship

--- VideoRentalCodeFirst/Model/Video.cs
+++ VideoRentalCodeFirst/Model/Video.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using VideoRentalCodeFirst.Enums;
 
@@ -6,11 +7,16 @@ namespace VideoRentalCodeFirst.Model
 {
     public class Video
     {
+        public Video()
+        {
+            Tags=new List<Tag>();
+        }
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
         public virtual Genre Genre { get; set; }
         public int GenreId { get; set; }
         public virtual VideoClassification? Classification { get; set; }
+        public virtual ICollection<Tag> Tags { get; set; }
     }  
 }
