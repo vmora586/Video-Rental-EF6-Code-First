d4c894bc75090f523b35e7465667f6a22321b8c3
Victor M <victomc.586@gmail.com>
2020-07-04 15:59:00 -0500

    populated Genre table

---
+++ VideoRentalCodeFirst/Migrations/202007042055245_PopulateGenreTable1.cs
@@ -0,0 +1,28 @@
+﻿namespace VideoRentalCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateGenreTable1 : DbMigration
+    {
+        public override void Up()
+        {
+			            Sql(@"
+			INSERT INTO Genres (Id, Name)
+			VALUES 
+				(1, 'Comedy'), 
+				(2, 'Action'), 
+				(3, 'Horror'), 
+				(4, 'Thriller'), 
+				(5, 'Family'), 
+				(6, 'Romance'),
+			    (7, 'Drama')
+			");		
+        }
+        
+        public override void Down()
+        {
+	        Sql("DELETE FROM Genres WHERE Id BETWEEN 1 AND 6");
+        }
+    }
+}
